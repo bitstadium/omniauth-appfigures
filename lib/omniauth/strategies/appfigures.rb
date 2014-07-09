5127c71cbb2920cf7dad5a608f989075ce833cd4
Marc Schopper <marc@hockeyapp.net>
2014-07-09 16:01:48 +0200

    Handle OAuth::Unauthorized for wrong/missing client_id or client_secret

--- lib/omniauth/strategies/appfigures.rb
+++ lib/omniauth/strategies/appfigures.rb
@@ -3,62 +3,50 @@ require 'multi_json'
 
 module OmniAuth
   module Strategies
-
     class AppFigures < OmniAuth::Strategies::OAuth
-      
-      option :name, 'appFigures'
-      
+      option :name, 'appfigures'
+      option :failure_path, '/' 
+
       option :client_options, {
         :access_token_path => "/v2/oauth/access_token",
         :authorize_path => "/v2/oauth/authorize",
         :request_token_path => "/v2/oauth/request_token",
         :site => "https://api.appfigures.com"
       }
-      
-      uid { 
+
+      uid {
         user_info['id']
       }
-      
-      info do 
+
+      info do
         {
-          #:nickname => user_info['username'],
-          :email => user_info['email'],
-          #:name => user_info['fullname'],
-          #:first_name => user_info['firstname'],
-          #:last_name => user_info['lastname'],
-          #:description => user_info['about'],
-          #:image => user_info['userpic_url'],
-          #:urls => {
-          #  '500px' => user_info['domain'],
-          #  'Website' => user_info['contacts']['website']
-          #}
+          :email => user_info['email']
         }
       end
-      
+
       extra do
         {
           :raw_info => raw_info
         }
       end
 
-      # Return info gathered from the flickr.people.getInfo API call 
-     
       def raw_info
         @raw_info ||= MultiJson.decode(access_token.get('/v2/').body)
       rescue ::Errno::ETIMEDOUT
         raise ::Timeout::Error
       end
 
-      # Provide the "Person" portion of the raw_info
-      
       def user_info
         @user_info ||= raw_info.nil? ? {} : raw_info["user"]
       end
-      
+
       def request_phase
         options[:authorize_params] = {:perms => options[:scope]} if options[:scope]
         super
+      rescue ::OAuth::Unauthorized
+        redirect options.failure_path
       end
     end
   end
 end
+
